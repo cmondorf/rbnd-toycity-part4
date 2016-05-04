dd90426067009c64fafa929c59d40fa33003d456
Christian Mondorf <christian@mondorf.dk>
2016-05-04 12:52:25 +0200

    added .gitignore

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -2,8 +2,14 @@ require_relative 'find_by'
 require_relative 'errors'
 require 'csv'
 
-# Methods that interact with the database 
+# Methods that interact with the database
 
 class Udacidata
   # Your code goes here!
+
+  def create(options = {})
+    
+  end
+
+
 end
