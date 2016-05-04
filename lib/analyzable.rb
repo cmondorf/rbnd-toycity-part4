31811bd81d0f70028a9be558d522aadb0f848d5e
Christian Mondorf <christian@mondorf.dk>
2016-05-04 12:33:31 +0200

    faker added and working. Project baseline.

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,10 @@
+
+# Methods that analyse product data
+
 module Analyzable
   # Your code goes here!
+
+  # count_by_brand
+
+
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -2,6 +2,8 @@ require_relative 'find_by'
 require_relative 'errors'
 require 'csv'
 
+# Methods that interact with the database 
+
 class Udacidata
   # Your code goes here!
 end
