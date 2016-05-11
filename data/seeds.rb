599e79bbc44e608ba1a4e1b6c8b1e9863fc95349
Christian Mondorf <christian@mondorf.dk>
2016-05-11 13:45:17 +0200

    intermediate save
    everything working until
    test_destroy_method_removes_product_from_database

--- data/seeds.rb
+++ data/seeds.rb
@@ -3,6 +3,11 @@ require 'faker'
 # This file contains code that populates the database with
 # fake data for testing purposes
 
-def db_seed
-  # Your code goes here!
+
+def db_seed # tests run noticeably slower since adding this
+  5.times do
+  Product.create( brand: Faker::Company.name,
+                  name: Faker::Commerce.product_name,
+                  price: Faker::Commerce.price )
+	end
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,86 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
-end
+  create_finder_methods :brand, :name
+  @@database_path = File.dirname(__FILE__) + "/../data/data.csv"
+  def self.create(options={})
+      product_in_database = self.all.select{|prod| prod.name == options[:name] && prod.brand == options[:brand] && prod.price == options[:price]}.first
+      if product_in_database
+        @product = product_in_database
+      else
+        @product = Product.new(options)
+      end
+      @product = Product.new(options)
+    	CSV.open(@@database_path, "a+") do |csv|
+    	 csv << [@product.id, @product.brand, @product.name, @product.price]
+    	end
+      return @product
+  end
+
+  def self.all
+     all_products = []
+     database_content = CSV.read(@@database_path).drop(1)
+     database_content.each do |item|
+       all_products << Product.new({id: item[0], brand: item[1], name: item[2], price:item[3]})
+     end
+     return all_products
+   end
+
+   #Product.first should return a Product object that represents the first product in the database.
+   #Product.first(n) will return an array of Product objects for the first n products in the database.
+
+   def self.first(number = 1)
+     if number <= 1
+       database_content = CSV.read(@@database_path)[1]
+       first_product= Product.new({id: database_content[0], brand: database_content[1], name: database_content[2], price:database_content[3]})
+       return first_product
+     else
+       products = []
+       counter = 1 # set to 1 to skip header
+       while counter < number+1 do # added +1 to ensure that method doesn't return n-1 results
+         database_content = CSV.read(@@database_path)[counter]
+         products << Product.new({id: database_content[0], brand: database_content[1], name: database_content[2], price:database_content[3]})
+         counter += 1
+       end
+     end
+    return products
+   end
+
+   def self.last(number=1)
+     array_of_products = Array.new
+     array_of_products = self.all
+     if number== 1
+       return array_of_products.last
+     else
+       new_array = Array.new
+       counter = 0
+       while counter < number
+         new_array << array_of_products.reverse[counter]
+         counter += 1
+       end
+       return new_array
+     end
+   end
+
+   def self.find(id)
+     array_of_products = self.all
+     return array_of_products.each {|item| return item if item.id == id}
+     #a.select {|item|"a" == item}
+   end
+
+   def self.destroy(id)
+     array_of_products = self.all
+     to_destroy = self.find(id)
+     array_of_products.delete_if{|item| item.id==to_destroy.id}
+     #rewrite to CSV
+     CSV.open(@@database_path, "wb") do |csv| #db_create all over again
+      csv << ["id", "brand", "product", "price"]
+        array_of_products.each do |item|
+          csv << [item.id, item.brand, item.name, item.price]
+        end
+      end
+   end
+
+
+
+  end
